949e3e03a2d01c9ea708193907884ee42f11102b
Kate-13 <kate_boyko@mail.ru>
2020-10-27 16:17:04 +0300

    feat: implement function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 
 module.exports = function countCats(backyard) {
   let catCount = 0;
-  for(i = 0; i < backyard.length; i++){
+  for(let i = 0; i < backyard.length; i++){
     for(j = 0; j < backyard[i].length; j++){
       if(backyard[i][j] === '^^'){
         catCount = catCount + 1;
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let teamName = '';
+  let letterarr = [];
+  if(members && members.length !== 0 && Array.isArray(members) === true){
+    for(let i = 0; i < members.length; i++){
+      if(typeof members[i] === "string"){
+        let name = members[i];
+        let l = name.trim().slice(0,1).toUpperCase();
+        letterarr.push(l);
+        teamName = letterarr.sort().join('');
+      };
+    };
+    if(teamName !== ''){
+      return teamName;
+    };
+  } else return false;
+}
