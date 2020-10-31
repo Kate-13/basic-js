45ff5f7dd9e26924be9deec2e7eae51ef78b5f19
Kate-13 <kate_boyko@mail.ru>
2020-11-01 02:51:30 +0300

    feat: implement chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  resultChain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.resultChain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(String(value)){
+      this.resultChain.push(String(value));
+    } else {
+      this.resultChain.push('');
+    }
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position > 0 && position <= this.resultChain.length){
+      this.resultChain.splice(position - 1, 1);
+      } else {
+        this.resultChain = [];
+        throw new Error();
+      }
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.resultChain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+    for (let i = 0; i < this.resultChain.length - 1; i++){
+      str += '( ' + this.resultChain[i] + ' )~~';
+    }
+    if(this.resultChain.length > 0) {
+      str += '( ' + this.resultChain[this.resultChain.length - 1] + ' )';
+    }
+    this.resultChain = [];
+    return str;
   }
 };
 
