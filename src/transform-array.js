7fd3e71df3802492901424b8b131f3b298af1b52
Kate-13 <kate_boyko@mail.ru>
2020-10-31 23:28:34 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr) === false){
+    throw Error();
+  }
+  newarr = [];
+  for( let i = 0; i < arr.length; i++) {
+    if(arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-prev' && arr[i] !== '--double-next') {
+      if(arr[i-1] !== '--discard-next' ){
+        newarr.push(arr[i]);
+      }
+      if(arr[i-1] === '--double-next') {
+        newarr.push(arr[i]);
+      }
+    } else if(arr[i] === '--discard-prev' && arr[i-2] !=='--discard-next' ) {
+      newarr.pop();
+    } else if(arr[i] === '--double-prev' && arr[i-2] !== '--discard-next' && i !== 0) {
+      newarr.push(arr[i-1]);
+    }
+  }
+  return newarr;
 };
