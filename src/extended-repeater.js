5687ecca5ae4edcc7cff03ac9e5246b944b8f65f
Kate-13 <kate_boyko@mail.ru>
2020-11-01 23:29:20 +0300

    feat: implement repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function repeater(str, options) {
+  let result = '';
+  let addition = '';
+  let addseparator = '';
+  let addresult = '';
+  let separator = '';
+  if(options.addition !== undefined){
+    addition = options.addition;
+      if(options.additionSeparator){
+        addseparator = options.additionSeparator;
+      } else if(!options.additionSeparator && options.addition) {
+      addseparator =  '|';
+        }
+    if(options.additionRepeatTimes > 1){
+      addresult = (addition + addseparator).repeat(options.additionRepeatTimes-1) + addition;
+    } else if(options.additionRepeatTimes = 1){
+      addresult = addition;
+    }
+  }else {
+    addresult = '';
+  }
+if(options.separator){
+  separator = options.separator;
+} else {
+  separator = '+';
+}
+if(options.repeatTimes > 1){
+  result = str + addresult + separator;
+  result = result.repeat(options.repeatTimes-1) + str + addresult;
+} else if(options.repeatTimes = 1){
+  result = str + addresult;
+}
+  return result;
 };
-  
