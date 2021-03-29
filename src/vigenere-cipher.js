2dc40d3d3857c47bdf87b42f57d1ddc0f6bca8b8
Kate-13 <kate_boyko@mail.ru>
2021-03-30 00:06:13 +0300

    feat: add VigenereCipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,78 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct) {
+    if(direct === undefined || direct === null) {
+      this.direct = true;
+    } else {
+      this.direct = direct;
+    }
+  }
+
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let key_index = 0;
+    let result = "";
+
+    for (var i = 0; i < message.length; i++) {
+      let index_message = characters.indexOf(message[i]);
+      if(index_message >= 0) {
+        let index_key = characters.indexOf(key[key_index]);
+        let index = ( ( index_message + index_key ) % characters.length );
+        result += characters[index];
+
+        if( (key_index + 1) == key.length) {
+          key_index = 0;
+        } else {
+          key_index = key_index + 1;
+        }
+
+      } else {
+        result += message[i];
+      }
+
+    }
+
+    if(!this.direct) {
+      result = result.split("").reverse().join("")
+    }
+
+    return result;
+  }
+  decrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let key_index = 0;
+    let result = "";
+
+    for (var i = 0; i < message.length; i++) {
+      let index_message = characters.indexOf(message[i]);
+      if(index_message >= 0) {
+        let index_key = characters.indexOf(key[key_index]);
+        let index = ( ( index_message + characters.length - index_key ) % characters.length );
+        result += characters[index];
+
+        if( (key_index + 1) == key.length) {
+          key_index = 0;
+        } else {
+          key_index = key_index + 1;
+        }
+
+      } else {
+        result += message[i];
+      }
+
+    }
+
+    if(!this.direct) {
+      result = result.split("").reverse().join("")
+    }
+
+    return result;
   }
 }
 
