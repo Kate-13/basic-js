9055cd0b433d27e6fbe2479a3c8348ef5b543cc6
Kate-13 <kate_boyko@mail.ru>
2021-03-30 20:18:16 +0300

    feat: update carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,7 +5,7 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   let result;
-  if(( typeof sampleActivity == 'string') && (typeof Number(sampleActivity) =='number') && !isNaN(sampleActivity) && sampleActivity && Number.isInteger(Number(sampleActivity))){
+  if(( typeof sampleActivity == 'string') && (typeof Number(sampleActivity) =='number') && !isNaN(sampleActivity) && sampleActivity && Number.isInteger(Number(sampleActivity)) && (typeof value !== 'undefined')){
     let k = 0.693/HALF_LIFE_PERIOD;
     let t = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
     result = t;
